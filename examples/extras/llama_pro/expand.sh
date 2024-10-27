b48888ab6d2323c5557287dd0cee50a2a4efe66d
tenacious <tenacioustommy6@gmail.com>
2024-10-27 08:55:52 +0000

    update di

--- examples/extras/llama_pro/expand.sh
+++ examples/extras/llama_pro/expand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python scripts/llama_pro.py \
-    --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
+    --model_name_or_path /kaggle/input/llama-3.2/transformers/3b-instruct/1 \
     --output_dir models/Llama-3.2-3B-Instruct-pro \
     --num_expand 8
